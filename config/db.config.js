dd57e1cf8fd0964646162dc4a389e510acd8df2c
pkecastillo <pkecastillo@gmail.com>
2022-01-04 11:02:06 +0100

    Ajustando cambios

---
+++ config/db.config.js
@@ -0,0 +1,13 @@
+const { createPool } = require("mysql");
+/** Connection pool creation - START */
+const db = createPool({
+  port: 3306,
+  host: "127.0.0.1",
+  user: "root",
+  password: "9121088",
+  database: "heroku",
+  connectionLimit: 10,
+});
+/** Connection pool creation - END */
+
+module.exports = db;
